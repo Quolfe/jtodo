c1a49d4a8b9a97318c964588d8ce1b79cb03452a
Ethan <edjordan20@outlook.com>
2026-06-24 07:39:55 -0500

    Edit Task field name for consistency

--- src/task.rs
+++ src/task.rs
@@ -5,22 +5,22 @@ pub struct Task {
     name: String,
     description: String,
     priority: usize,
-    due_time: usize,
+    time_due: usize,
     duration: Duration,
     parent_id: Option<usize>,
     subtask_ids: Vec<usize>,
 }
 
 impl Task {
-    pub fn new(id: usize, name: String, description: String, priority: usize, due_time: usize, duration: Duration, parent_id: Option<usize>, subtask_ids: Vec<usize>) -> Self {
-        Task { id, name, description, priority, due_time, duration, parent_id, subtask_ids, }
+    pub fn new(id: usize, name: String, description: String, priority: usize, time_due: usize, duration: Duration, parent_id: Option<usize>, subtask_ids: Vec<usize>) -> Self {
+        Task { id, name, description, priority, time_due, duration, parent_id, subtask_ids, }
     }
 
     pub fn id(&self) -> usize { self.id }
     pub fn name(&self) -> &str { &self.name }
     pub fn description(&self) -> &str { &self.description }
     pub fn priority(&self) -> usize { self.priority }
-    pub fn due_time(&self) -> usize { self.due_time }
+    pub fn time_due(&self) -> usize { self.time_due }
     pub fn duration(&self) -> Duration { self.duration }
     pub fn parent_id(&self) -> Option<usize> { self.parent_id }
     pub fn subtask_ids(&self) -> &[usize] { &self.subtask_ids }
