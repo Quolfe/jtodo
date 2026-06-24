d3c899d456b030c26f1e1d698a25df7f24c6de73
Ethan <edjordan20@outlook.com>
2026-06-24 07:35:54 -0500

    Expand Task fields

--- src/task.rs
+++ src/task.rs
@@ -1,19 +1,27 @@
+use std::time::Duration;
+
 pub struct Task {
     id: usize,
     name: String,
     description: String,
+    priority: usize,
+    due_time: usize,
+    duration: Duration,
+    parent_id: Option<usize>,
+    subtask_ids: Vec<usize>,
 }
 
 impl Task {
-    pub fn new(id: usize, name: String, description: String) -> Self {
-        Task {
-            id,
-            name,
-            description,
-        }
+    pub fn new(id: usize, name: String, description: String, priority: usize, due_time: usize, duration: Duration, parent_id: Option<usize>, subtask_ids: Vec<usize>) -> Self {
+        Task { id, name, description, priority, due_time, duration, parent_id, subtask_ids, }
     }
 
     pub fn id(&self) -> usize { self.id }
     pub fn name(&self) -> &str { &self.name }
     pub fn description(&self) -> &str { &self.description }
+    pub fn priority(&self) -> usize { self.priority }
+    pub fn due_time(&self) -> usize { self.due_time }
+    pub fn duration(&self) -> Duration { self.duration }
+    pub fn parent_id(&self) -> Option<usize> { self.parent_id }
+    pub fn subtask_ids(&self) -> &[usize] { &self.subtask_ids }
 }
